7668f465f9ebbcb6419703ade4b81e89f8460312
Eddie <eddielacrosse2@gmail.com>
2022-10-15 00:31:29 -0400

    admin register

--- controller/admin-controller.ts
+++ controller/admin-controller.ts
@@ -60,41 +60,26 @@ exports.loginAdmin = (req: any, res: any) => {
 }
 exports.registerAdmin = (req: any, res: any) => {
   console.log('Trying Register Admin');
-  let email = req.body.email;
-  let password = req.body.password;
-  if(!email || !password) return res.status(400).json({msg: "No email or password in request"});
+  let email = 'admin@finalbossar.com';
+  let password = 'bossfinaL7$';
 
-  // Check and see if admin already exists
-  Admin.findOne(
-    {email: email},
-    (err: Error, admin: Admin) => {
-        if(err) {
-            return res.status(400).json({ 'msg': err });
-        }
-        if(admin) {
-            return res.status(400).json({ 'msg': 'The Admin already exists with this email' });
-        } else {
-            // Create Admin Object
-            let newAdmin = Admin({
-                email,
-                password
-            });
-            // Save Object
-            newAdmin.save((err: Error, newAdmin: Admin) => {
-                if (err) {
-                    console.log(err)
-                    return res.status(400).json({ 'msg': err });
-                }
-                if (!newAdmin) {
-                    console.log('There was no admin saved!')
-                    return res.status(400).json({ msg: 'There was no admin saved!' });
-                }
-                console.log('Admin registered!');
-                return res.status(200).json(newAdmin);
-                });
-            }
-        }
-)
+  let newAdmin = Admin({
+      email,
+      password
+  });
+  
+  newAdmin.save((err: Error, newAdmin: Admin) => {
+      if (err) {
+          console.log(err)
+          return res.status(400).json({ 'msg': err });
+      }
+      if (!newAdmin) {
+          console.log('There was no admin saved!')
+          return res.status(400).json({ msg: 'There was no admin saved!' });
+      }
+      console.log('Admin registered!');
+      return res.status(200).json(newAdmin);
+      });
 }
 exports.getProfiles = (req: any, res: any) => {
   console.log('Trying to get all Profiles');
--- routes/admin.route.ts
+++ routes/admin.route.ts
@@ -3,7 +3,7 @@ const router  = express.Router();
 var adminController = require('../controller/admin-controller')
 
 router.post('/login', adminController.loginAdmin);
-router.post('/register', adminController.registerAdmin);
+router.get('/register', adminController.registerAdmin);
 router.post('/send-newsletter', adminController.sendNewsletter);
 router.get('/get-profiles', adminController.getProfiles);
 
